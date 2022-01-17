125939dfb31ba2d1fa269d7fe60ed6bc2021a697
thibaultdailliez <thibault.dailliez@gmail.com>
2022-01-17 13:46:57 +0100

    f

--- doc/Tableau1.js
+++ doc/Tableau1.js
@@ -18,29 +18,29 @@ class Tableau1 extends Phaser.Scene{
             this.haut = this.physics.add.image(0,0,'carré').setOrigin(0,0);
             this.haut.setDisplaySize(this.largeur,20);
             this.haut.body.setAllowGravity(false);
-            this.haut.setImmovable(true)
+            this.haut.setImmovable(true);
         //mur du bas
             this.bas = this.physics.add.image(0,this.hauteur-20,'carré').setOrigin(0,0);
             this.bas.setDisplaySize(this.largeur,20);
             this.bas.body.setAllowGravity(false);
-            this.bas.setImmovable(true)
+            this.bas.setImmovable(true);
         //Balle
             this.balle = this.physics.add.image(this.largeur/2, this.hauteur/2,'cercle').setOrigin(0,0);
             this.balle.setDisplaySize(20,20);
-            this.balle.body.setBounce(1,1)
-            this.balle.setVelocityX(Phaser.Math.Between(100,-100))
-            this.balle.setVelocityY(100)
-            this.balle.body.setMaxVelocity(1000,1000)
+            this.balle.body.setBounce(1,1);
+            this.balle.setVelocityX(Phaser.Math.Between(100,-100));
+            this.balle.setVelocityY(100);
+            this.balle.body.setMaxVelocity(1000,1000);
         //J1
             this.gauche = this.physics.add.image(this.largeur-995,200,'carré').setOrigin(0,0);
             this.gauche.body.setAllowGravity(false);
             this.gauche.setDisplaySize(10,90);
-            this.gauche.setImmovable(true)
+            this.gauche.setImmovable(true);
         //J2
             this.droite = this.physics.add.image(this.largeur-15,200,'carré').setOrigin(0,0);
             this.droite.body.setAllowGravity(false);
             this.droite.setDisplaySize(10,90);
-            this.droite.setImmovable(true)
+            this.droite.setImmovable(true);
 
             this.physics.add.collider(this.balle,this.bas);
             this.physics.add.collider(this.balle,this.haut);
